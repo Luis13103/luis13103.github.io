6cabd71874a36d660379441f25314de10438ebb5
Luis13103 <156386505+Luis13103@users.noreply.github.com>
2024-01-25 17:06:17 -0600

    Create login.js

---
+++ login.js
@@ -0,0 +1,30 @@
+function validarFormulario() {
+    var emailInput = document.getElementById('email');
+    var passwordInput = document.getElementById('password');
+
+    var email = emailInput.value;
+    var password = passwordInput.value;
+
+    // Validar correo
+    if (!validarCorreo(email)) {
+        alert('El correo debe contener el símbolo "@"');
+        return;
+    }
+
+    // Validar contraseña
+    if (!validarContraseña(password)) {
+        alert('La contraseña debe tener al menos 8 caracteres');
+        return;
+    }
+
+    // Continuar con el proceso de inicio de sesión si las validaciones son exitosas
+    // ...
+}
+
+function validarCorreo(email) {
+    return email.includes('@');
+}
+
+function validarContraseña(password) {
+    return password.length >= 8;
+}
